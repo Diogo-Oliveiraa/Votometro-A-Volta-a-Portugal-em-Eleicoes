3b2b256d939eb4f285ecf6ab45e467cf3c4ce323
Rodrigo Courela <rcourela2@gmail.com>
2025-05-25 12:24:22 +0100

    Adição da Abstenção

--- Simular_Votos.py
+++ Simular_Votos.py
@@ -4,9 +4,10 @@ import os
 
 def simular_votos(total_votos, partidos,pesos_partidos):
 
-    votos_nulos = int(total_votos * 0.02)     # 2%
-    votos_brancos = int(total_votos * 0.03)   # 3%
-    votos_validos = total_votos - votos_nulos - votos_brancos
+    votos_nulos = int(total_votos * 0.02) #2%
+    votos_brancos = int(total_votos * 0.03) #3%
+    abstencao = int(total_votos * 0.30) #30%
+    votos_validos = total_votos - votos_nulos - votos_brancos - abstencao
 
     distribuicao = {p: 0 for p in partidos}
     pesos = [pesos_partidos[p] for p in partidos]
@@ -16,7 +17,7 @@ def simular_votos(total_votos, partidos,pesos_partidos):
     for partido in votos_simulados:
         distribuicao[partido] += 1
 
-    return distribuicao, votos_nulos, votos_brancos
+    return distribuicao, votos_nulos, votos_brancos, abstencao,votos_validos
 
 
 def resultado_votos(distritos_concelhos, partidos, pesos_partidos):
@@ -27,13 +28,17 @@ def resultado_votos(distritos_concelhos, partidos, pesos_partidos):
         distrito = row["Distrito"]
         concelho = row["Concelho"]
         populacao = row["Inscritos"]
-        votos, votos_nulos, votos_brancos = simular_votos(populacao, partidos, pesos_partidos)
+
+        votos, votos_nulos, votos_brancos, abstencao, votos_validos  = simular_votos(populacao, partidos, pesos_partidos)
 
         resultado = {
             "Distrito": distrito,
             "Concelho": concelho,
+            "Inscritos": populacao,
+            "Votos Validos": votos_validos,
             "Votos Nulos": votos_nulos,
             "Votos Brancos": votos_brancos,
+            "Abstencao": abstencao,
         }
 
         resultado.update(votos)
