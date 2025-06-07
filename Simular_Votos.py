5b570cab3cf723bfb044ede1b6dcb23c75eac1fc
Rodrigo Courela <48653311+Courela98@users.noreply.github.com>
2025-06-07 15:05:43 +0100

    Update Simular_Votos.py

--- Simular_Votos.py
+++ Simular_Votos.py
@@ -1,60 +1,61 @@
+"""Biblioteca para dados random / guardar em ficheiro JSON / localizao do OS"""
 import random
 import json
 import os
 import pandas as pd
 
-def simular_votos(total_votos, partidos,pesos_partidos):
-
-    votos_nulos = int(total_votos * 0.02) #2%
-    votos_brancos = int(total_votos * 0.03) #3%
-    abstencao = int(total_votos * 0.30) #30%
-    votos_validos = total_votos - votos_nulos - votos_brancos - abstencao
-
+def simular_votos(total_votos, partidos):
+    """ Função para simular uma distribuição aleatoria dos votos"""
     distribuicao = {p: 0 for p in partidos}
-    pesos = [pesos_partidos[p] for p in partidos]
-
-    votos_simulados = random.choices(partidos, weights=pesos, k=votos_validos)
+    votos_simulados = random.choices(partidos, k=total_votos)
 
     for partido in votos_simulados:
         distribuicao[partido] += 1
 
-    return distribuicao, votos_nulos, votos_brancos, abstencao,votos_validos
+    return distribuicao
 
+def resultado_votos(distritos_concelhos, partidos):
+    """ Função para guardar os votos por concelho em ficheiro JSON e Excel """
+    os.makedirs("./ResultadoEleicoesDistritos/JSON", exist_ok=True)
+    os.makedirs("./ResultadoEleicoesDistritos/XLSX", exist_ok=True)
 
-def resultado_votos(distritos_concelhos, partidos, pesos_partidos):
-
-    resultadoscsv = []
-
-    for i, row in distritos_concelhos.iterrows():
+    for _, row in distritos_concelhos.iterrows():
         distrito = row["Distrito"]
         concelho = row["Concelho"]
         populacao = row["Inscritos"]
 
-        votos, votos_nulos, votos_brancos, abstencao, votos_validos  = simular_votos(populacao, partidos, pesos_partidos)
+        votos = simular_votos(populacao, partidos)
 
         resultado = {
             "Distrito": distrito,
             "Concelho": concelho,
             "Inscritos": populacao,
-            "VV": votos_validos,
-            "VN": votos_nulos,
-            "VB": votos_brancos,
-            "Abstencao": abstencao,
         }
 
         resultado.update(votos)
-        resultadoscsv.append(resultado)
+        resultado.pop("NaoVotantes",None)
 
-        filename = f"{distrito}_{concelho}.json"
-        caminho = os.path.join("./ResultadoEleicoesDistritos", filename)
+        """ Cria um ficheiro JSON com as votações dos partidos"""
+        filename = f"{concelho}.json"
+        caminho = os.path.join("./ResultadoEleicoesDistritos/JSON", filename)
 
-        with open(caminho, "w", encoding="utf-8") as f:
-            json.dump(resultado, f, indent=2, ensure_ascii=False)
+        with open(caminho, "w", encoding="utf-8") as file:
+            json.dump(resultado, file, indent=2, ensure_ascii=False)
 
-
+        """ Cria um ficheiro excel com as votações dos partidos"""
         filename_excel = f"{distrito}_{concelho}.xlsx"
-        caminho_excel = os.path.join("./ResultadoEleicoesDistritos", filename_excel)
+        caminho_excel = os.path.join("./ResultadoEleicoesDistritos/XLSX", filename_excel)
         df_resultado = pd.DataFrame([resultado])
         df_resultado.to_excel(caminho_excel, index=False)
 
-    return resultadoscsv
+
+def main():
+    """Simulação a partir dos ficheiros de entrada"""
+    df_partidos = pd.read_excel("./Docs/partidos.xlsx")
+    partidos = df_partidos["Partidos"].tolist()
+    distritos_concelhos = pd.read_excel("./Docs/Distritos_Concelhos.xlsx")
+    resultado_votos(distritos_concelhos, partidos)
+
+
+if __name__ == "__main__":
+    main()
