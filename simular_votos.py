347164561549f31084cfff58a80080c3f07417bb
Rodrigo Courela <rcourela2@gmail.com>
2025-06-10 11:47:21 +0100

    Atualização codigo

--- simular_votos.py
+++ simular_votos.py
@@ -6,9 +6,12 @@ import pandas as pd
 
 def simular_votos(total_votos, partidos):
     """ Função para simular uma distribuição aleatoria dos votos"""
+    distribuicao = {p: 0 for p in partidos}
     votos_simulados = random.choices(partidos, k=total_votos)
-    return {p: votos_simulados.count(p) for p in partidos}
 
+    for partido in votos_simulados:
+        distribuicao[partido] += 1
+    return distribuicao
 
 def criar_pastas():
     """ Criação das pastas para guardar os ficheiros"""
@@ -17,7 +20,6 @@ def criar_pastas():
         os.makedirs("./ResultadoEleicoesDistritos/XLSX", exist_ok=True)
     except OSError as erro:
         print(f"Pastas não criadas! {erro}")
-        return
 
 def resultado_votos(distritos_concelhos, partidos):
     """ Função para guardar os votos por concelho em ficheiro JSON e Excel """
@@ -39,14 +41,14 @@ def resultado_votos(distritos_concelhos, partidos):
         resultado.update(votos)
         resultado.pop("NaoVotantes",None)
 
-        """ Cria um ficheiro JSON com as votações dos partidos"""
+        #Cria um ficheiro JSON com as votações dos partidos
         filename = f"{concelho}.json"
         caminho = os.path.join("./ResultadoEleicoesDistritos/JSON", filename)
 
         with open(caminho, "w", encoding="utf-8") as jsonfile:
             json.dump(resultado, jsonfile, indent=2, ensure_ascii=False)
 
-        """ Cria um ficheiro excel com as votações dos partidos"""
+        #Cria um ficheiro excel com as votacoes dos partidos
         filename_excel = f"{distrito}_{concelho}.xlsx"
         caminho_excel = os.path.join("./ResultadoEleicoesDistritos/XLSX", filename_excel)
         df_resultado = pd.DataFrame([resultado])
